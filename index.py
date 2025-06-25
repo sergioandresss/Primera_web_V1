0a902cbf0cd5a9d9a9fd73e00203d7ed7f254ee4
sergioandresss <sergioandresmunozescobedo790@gmail.com>
2025-06-25 19:08:32 -0400

    agrege carpeta tamples y index html

---
+++ index.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route ("/about")
+def about():
+    return "About page"
+
+
+if __name__ == "__main__":
+    app.run()
